324115df43887f9ecd82f88c7799cfef8e2df10c
jayanth <jayanthp17@gmail.com>
2024-05-22 22:33:49 +0530

    setup

---
+++ presidio-challenge-backend/src/test/java/com/presidio/challenge/PresidioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.presidio.challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PresidioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
